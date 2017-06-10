5d36a147db561c67565bdcca73a95219ad4e5bf8
Tiago Castro Henriques <trinosauro@gmail.com>
2017-06-10 23:09:03 +0200

    Reformat code

--- tic-tac-toe-demo/main.js
+++ tic-tac-toe-demo/main.js
@@ -1,9 +1,8 @@
-jQuery(document).ready(function($){
-    window.squares = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
-    $('.square').on('click',function(){
-        var i = $(this).data('id');
-        squares[i] = 1;
-        $(this).addClass('x');
-    });
-
+jQuery(document).ready(function($) {
+  window.squares = [-1, -1, -1, -1, -1, -1, -1, -1, -1]
+  $(".square").on("click", function() {
+    var i = $(this).data("id")
+    squares[i] = 1
+    $(this).addClass("x")
+  })
 })
